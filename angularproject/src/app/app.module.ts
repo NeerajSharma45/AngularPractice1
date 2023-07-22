2f129f46d88ee245245c1934198f532eb50b0e13
neeraj-sharma123 <neeraj.sharma@accolitedigital.com>
2023-07-22 13:35:35 +0530

    Login Form Created

--- angularproject/src/app/app.module.ts
+++ angularproject/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angularproject/src/app/components/login-form/login-form.component.ts
+++ angularproject/src/app/components/login-form/login-form.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-form.component.css']
 })
 export class LoginFormComponent {
+  formData = {
+    name: '',
+    email: '',
+    number: '',
+    password: ''
+  };
 
+  submitForm() {
+    if(this.isLogin)
+    alert("You are Logged In");
+    else
+    alert("You are signed up");
+  }
+  isLogin:boolean=true;
+  form_name:String="Login";
+  login(){
+    this.isLogin=true;
+    this.form_name="Login";
+    console.log(this.form_name);
+  }
+  signUp(){
+    this.isLogin=false;
+    this.form_name="SignUp";
+    console.log(this.form_name);
+  }
 }
