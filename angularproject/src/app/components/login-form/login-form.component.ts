e5fd8a7349b79a9b7647883636efd202eca16f29
neeraj-sharma123 <neeraj.sharma@accolitedigital.com>
2023-07-22 15:14:38 +0530

    Login Form Updated

--- angularproject/src/app/components/login-form/login-form.component.ts
+++ angularproject/src/app/components/login-form/login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
@@ -12,23 +13,47 @@ export class LoginFormComponent {
     number: '',
     password: ''
   };
-
-  submitForm() {
+  reset(){
+    this.formData.name='';
+    this.formData.email='';
+    this.formData.password='';
+    this.formData.number='';
+  }
+  checkAllDetailsFilled():boolean{
+    if(this.isLogin){
+      if(this.formData.name!==''&&this.formData.email!==''&&this.formData.number!==''&&this.formData.password!=='')
+      return false; 
+      else
+      return true;
+    }
+    else{
+      if(this.formData.email!==''&&this.formData.password!=='')
+      return false;
+      else
+      return true;
+    }
+  }
+  submitForm(form:NgForm) {
+    if(this.checkAllDetailsFilled()){
+      alert("Please Enter all details");
+      return;
+    }
     if(this.isLogin)
     alert("You are Logged In");
     else
     alert("You are signed up");
+    this.reset();
   }
   isLogin:boolean=true;
   form_name:String="Login";
-  login(){
+  login(form:NgForm){
+    this.reset();
     this.isLogin=true;
     this.form_name="Login";
-    console.log(this.form_name);
   }
-  signUp(){
+  signUp(form:NgForm){
+    this.reset();
     this.isLogin=false;
     this.form_name="SignUp";
-    console.log(this.form_name);
   }
 }
