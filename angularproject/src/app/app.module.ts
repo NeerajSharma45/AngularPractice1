c7fb25623d8a354418ab3d14f2901b045632fd93
shoaibs01 <shoaibanwar.shaikh@accolitedigital.com>
2023-07-24 16:48:09 +0530

    added login and register

--- angularproject/src/app/app.module.ts
+++ angularproject/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginFormComponent } from './components/login-form/login-form.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -11,16 +10,19 @@ import { FormsModule } from '@angular/forms';
 import { ProductCategoryComponent } from './components/product-category/product-category.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginFormComponent,
     NavbarComponent,
     FooterComponent,
     ProductCategoryComponent,
     ProductListComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
