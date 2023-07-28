d7a64f37fd9804d47e75bfb690c7043f28a6edb9
shoaibs01 <shoaibanwar.shaikh@accolitedigital.com>
2023-07-29 00:02:02 +0530

    added cart service

--- angularproject/src/app/components/product-category/product-category.component.ts
+++ angularproject/src/app/components/product-category/product-category.component.ts
@@ -1,5 +1,5 @@
 import { Component, HostListener, OnInit } from '@angular/core';
-import { ProductCategory } from '../../models/productCategory';
+import { ProductCategory } from '../../models/product-category';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
